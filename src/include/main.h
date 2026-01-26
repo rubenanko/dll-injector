b01166786107a45bd2b8632fe3934ca21c2b8ff3
rubenanko <rubenanko@gmail.com>
2026-01-26 14:31:55 +0100

    first commit

---
+++ src/include/main.h
@@ -0,0 +1,8 @@
+#ifndef __DLL_INJECTOR_MAIN__
+#define __DLL_INJECTOR_MAIN__
+
+#include <windows.h>
+
+DWORD APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "include/main.h"
+
+DWORD APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+    return 0;
+}
