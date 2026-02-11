e69f29cf8cc278ca0c17422a8a7cfe3b319ba0d5
Ryan Bouchou <ryanbouchou@orange.fr>
2026-02-11 20:20:05 +0100

    Add(pe-parser): rvaToPtr()

--- src/utils/stdio-sec.c
+++ src/utils/stdio-sec.c
@@ -1,5 +1,14 @@
 #include <utils/stdio-sec.h>
 
+/**
+ * seek_checked - safely seeks to a specific offset in a file
+ *
+ * @fp: pointer to the file stream
+ * @offset: the offset to seek to
+ * @size: the total size of the file
+ *
+ * Returns true if the seek was successful, false otherwise
+ */
 bool seek_checked(FILE *fp, long offset, long size) {
   if (offset < 0 || offset > size) {
     return false;
